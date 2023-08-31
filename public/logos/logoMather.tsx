9664a81ba754eaa19a0310ca9a7473bc7856f83e
Aint <pinevichv@gmail.com>
2023-08-31 13:01:16 +0300

    updated eslint conf

--- public/logos/logoMather.tsx
+++ public/logos/logoMather.tsx
@@ -1,7 +1,5 @@
 import WindowsLogo from './windows/WindowsLogo';
 import BrowserLogo from './browser/BrowserLogo';
-import React from 'react';
-
 
 const platforms = {
     'PC (Windows)': <WindowsLogo externalClassName={'item-card__logo'}/>,
@@ -13,6 +11,5 @@ const platforms = {
 };
 
 export type platformNames = keyof typeof platforms;
-export type platformLogos = typeof platforms[platformNames];
 
 export const mathLogo = (platform: platformNames) => platforms[platform];
